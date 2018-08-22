5a670d2344df3ec1b6e4835102d1f07fa1fd9bcf
braj011 <bhagyaraj01@gmail.com>
2018-08-22 13:22:55 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,6 +17,7 @@ def first_challenge
   contacts["Freddy Mercury"].collect do |attribute, value|
     if attribute == :favorite_icecream_flavors
       value.delete("strawberry")
+      value - ["strawberry"]
     end 
   end
   contacts
@@ -25,6 +26,8 @@ end
 so is that basically because we had to delete the value
 and not actually edit the key which was immutable anyway, i.e. the ice cream fkavour 
 okay sure, i did see that when i got to that one, but wasn;t sure what to then delete
+
+just want to try this method here...
 
 
 
